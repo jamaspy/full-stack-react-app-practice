c7d8c2a1d0e5c9104fabf6320431e80a3902addc
James Aspinall <aspin@me.com>
2019-07-22 15:04:08 +1000

    OpenWeatherApi Working

--- server/models/weather.js
+++ server/models/weather.js
@@ -3,17 +3,17 @@ const request = require('request-promise');
 const API_KEY = 'f48c181814c3befb9177e85c1b8e2644'
 
 class Weather {
-    retrieveByCity (city, callback) {
+    static retrieveByCity (city, callback) {
         request({
             uri: `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}&units=metric`,
             json: true
-        }).then(function(res) {
+        }).then(function (res) {
             callback(res);
         }).catch(function (err){
             console.log(err);
-            callback({error: "Could not reach OpemWeatherMap API."});
+            callback({error: "Could not reach OpenWeatherMap API."});
         })    
     }
 }
 
-modules.exports = Weather;
+module.exports = Weather;
