82880d44953138c7e7b58168ce82fa5a8887a895
James Aspinall <aspin@me.com>
2019-08-01 08:29:23 +1000

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,115 +1,125 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 import {
-  Container,
-  Row,
-  Jumbotron,
-  InputGroup,
-  InputGroupAddon,
-  Button,
-  FormGroup,
-  Input,
-  Col
-} from 'reactstrap';
+	Container,
+	Row,
+	Jumbotron,
+	InputGroup,
+	InputGroupAddon,
+	Button,
+	FormGroup,
+	Input,
+	Col
+} from "reactstrap";
 
-import Weather from './components/weather';
+import Weather from "./components/weather";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-  
-    this.state = {
-       weather: null,
-       cityList: [],
-       newCityName: ''
-    };
-  }
+	constructor(props) {
+		super(props);
 
-  getCityList = () => {
-    fetch('/api/cities')
-    .then(res => res.json())
-    .then(res => {
-      var cityList = res.map(r => r.city_name);
-      this.setState({ cityList });
-    });
-  };
+		this.state = {
+			weather: null,
+			cityList: [],
+			newCityName: ""
+		};
+	}
 
-  handleInputChange = (e) => {
-    this.setState({ newCityName: e.target.value });
-  };
+	getCityList = () => {
+		fetch("/api/cities")
+			.then(res => res.json())
+			.then(res => {
+				var cityList = res.map(r => r.city_name);
+				this.setState({ cityList });
+			});
+	};
 
-  handleAddCity = () => {
-    fetch('/api/cities', {
-      method: 'post',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ city: this.state.newCityName })
-    })
-    .then(res => res.json())
-    .then(res => {
-      this.getCityList();
-      this.setState({ newCityName: '' });
-    });
-  };
+	handleInputChange = e => {
+		this.setState({ newCityName: e.target.value });
+	};
 
-  getWeather = (city) => {
-    fetch(`/api/weather/${city}`)
-    .then(res => res.json())
-    .then(weather => {
-      console.log(weather);
-      this.setState({ weather });
-    });
-  }
+	handleAddCity = () => {
+		fetch("/api/cities", {
+			method: "post",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({ city: this.state.newCityName })
+		})
+			.then(res => res.json())
+			.then(res => {
+				this.getCityList();
+				this.setState({ newCityName: "" });
+			});
+	};
 
-  handleChangeCity = (e) => {
-    this.getWeather(e.target.value);
-  }
+	getWeather = city => {
+		fetch(`/api/weather/${city}`)
+			.then(res => res.json())
+			.then(weather => {
+				console.log(weather);
+				this.setState({ weather });
+			});
+	};
 
-  componentDidMount () {
-    this.getCityList();
-  }
+	handleChangeCity = e => {
+		this.getWeather(e.target.value);
+	};
 
-  render() {
-    return (
-      <Container fluid className="centered bgImg">
-        <div className="box">
+	componentDidMount() {
+		this.getCityList();
+	}
 
-        <Row>
-          <Col>
-            
-              <h1 className="display-3 mt-5">MyWeather</h1>
-              <p className="lead">The current weather for your favorite cities</p>
-              <InputGroup>
-                <Input 
-                  placeholder="Add A City To Your Favourites"
-                  value={this.state.newCityName}
-                  onChange={this.handleInputChange}
-                />
-                <InputGroupAddon addonType="append">
-                  <Button outline color="secondary" onClick={this.handleAddCity}>Add City</Button>
-                </InputGroupAddon>
-                
-              </InputGroup>
-            
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <h1 className="display-5 mt-5">Favourite Cities</h1>
-            <FormGroup>
-              <Input type="select" onChange={this.handleChangeCity}>
-                { this.state.cityList.length === 0 && <option>No cities added yet.</option> }
-                { this.state.cityList.length > 0 && <option>Select a city.</option> }
-                  {/* SAVE THIS LINE OF CODE SOMEWHERE IT IS HANDY FOR MAPPING LIST */}
-                { this.state.cityList.map((city, i) => <option key={i}>{city}</option>) }
-              </Input>
-            </FormGroup>
-          </Col>
-        </Row>
-        </div>
-        <Weather data={this.state.weather}/>
-      </Container>
-    );
-  }
+	render() {
+		return (
+			<Container fluid className='centered bgImg'>
+				<div className='box'>
+					<Row>
+						<Col>
+							<h1 className='display-3 mt-5'>MyWeather</h1>
+							<p className='lead'>
+								The current weather for your favorite cities
+							</p>
+							<InputGroup>
+								<Input
+									placeholder='Add A City To Your Favourites'
+									value={this.state.newCityName}
+									onChange={this.handleInputChange}
+								/>
+								<InputGroupAddon addonType='append'>
+									<Button
+										outline
+										color='secondary'
+										onClick={this.handleAddCity}
+									>
+										Add City
+									</Button>
+								</InputGroupAddon>
+							</InputGroup>
+						</Col>
+					</Row>
+					<Row>
+						<Col>
+							<h1 className='display-5 mt-5'>Favourite Cities</h1>
+							<FormGroup>
+								<Input type='select' onChange={this.handleChangeCity}>
+									{this.state.cityList.length === 0 && (
+										<option>No cities added yet.</option>
+									)}
+									{this.state.cityList.length > 0 && (
+										<option>Select a city.</option>
+									)}
+									{/* SAVE THIS LINE OF CODE SOMEWHERE IT IS HANDY FOR MAPPING LIST */}
+									{this.state.cityList.map((city, i) => (
+										<option key={i}>{city}</option>
+									))}
+								</Input>
+							</FormGroup>
+						</Col>
+					</Row>
+				</div>
+				<Weather data={this.state.weather} />
+			</Container>
+		);
+	}
 }
 
 export default App;
